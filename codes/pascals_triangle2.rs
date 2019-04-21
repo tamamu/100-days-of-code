d86e3a9b2ebf7f739d864f0f369374e894f1be82
tamamu <tamamu.1r1s@gmail.com>
2019-04-21 18:13:57 +0900

    To be more efficient

--- codes/pascals_triangle2.rs
+++ codes/pascals_triangle2.rs
@@ -1,26 +1,39 @@
-static mut MEMO: [[i32; 34]; 34] = [[0; 34]; 34];
+// static mut MEMO: [[i32; 34]; 34] = [[0; 34]; 34];
 struct Solution {}
 impl Solution {
     pub fn get_row(row_index: i32) -> Vec<i32> {
-        let mut result = Vec::new();
-        for j in 0..row_index + 1 {
-            result.push(Solution::pascal_number(row_index as usize, j as usize));
+        let row_indexu = row_index as usize;
+        let mut result = vec![1];
+        if row_index > 0 {
+            result.resize(row_indexu + 1, 0);
+            let upper_row = Solution::get_row(row_index - 1);
+            for j in 1..row_indexu {
+                result[j] = upper_row[j - 1] + upper_row[j];
+            }
+            result[row_indexu] = 1;
         }
         result
     }
-    pub fn pascal_number(j: usize, k: usize) -> i32 {
-        unsafe {
-            if MEMO[j][k] == 0 {
-                if j == 0 || k == 0 || j == k {
-                    MEMO[j][k] = 1;
-                } else {
-                    MEMO[j][k] =
-                        Solution::pascal_number(j - 1, k - 1) + Solution::pascal_number(j - 1, k);
-                }
-            }
-            MEMO[j][k]
-        }
-    }
+    // pub fn get_row(row_index: i32) -> Vec<i32> {
+    //     let mut result = Vec::new();
+    //     for j in 0..row_index + 1 {
+    //         result.push(Solution::pascal_number(row_index as usize, j as usize));
+    //     }
+    //     result
+    // }
+    // pub fn pascal_number(j: usize, k: usize) -> i32 {
+    //     unsafe {
+    //         if MEMO[j][k] == 0 {
+    //             if j == 0 || k == 0 || j == k {
+    //                 MEMO[j][k] = 1;
+    //             } else {
+    //                 MEMO[j][k] =
+    //                     Solution::pascal_number(j - 1, k - 1) + Solution::pascal_number(j - 1, k);
+    //             }
+    //         }
+    //         MEMO[j][k]
+    //     }
+    // }
 }
 
 fn main() {
