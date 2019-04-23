41ea696b3f6f26b54c984364ed58194ec43ade95
tamamu <tamamu.1r1s@gmail.com>
2019-04-23 15:03:03 +0900

    Experiment: Is `n % 1 == 1` equivalent to `n & 1 == 1`?

---
+++ experiments/mod/and.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int x = 7;
+  if (x & 1 == 1) {
+    printf("odd");
+  }
+  return 0;
+}
---
+++ experiments/mod/percent.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int x = 7;
+  if (x % 2 == 1) {
+    printf("odd");
+  }
+  return 0;
+}
