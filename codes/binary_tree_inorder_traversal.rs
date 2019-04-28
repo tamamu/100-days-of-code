48eddc8fee3fef0254daf6b3618029a02779d276
tamamu <tamamu.1r1s@gmail.com>
2019-04-28 15:47:59 +0900

    Solve https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/

---
+++ codes/binary_tree_inorder_traversal.rs
@@ -0,0 +1,55 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+struct Solution {}
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn inorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut result = Vec::new();
+        if let Some(node) = root {
+            let borrowed = node.borrow();
+            let mut left = Solution::inorder_traversal(borrowed.left.clone());
+            for v in left.drain(0..) {
+                result.push(v);
+            }
+            result.push(borrowed.val);
+            let mut right = Solution::inorder_traversal(borrowed.right.clone());
+            for v in right.drain(0..) {
+                result.push(v);
+            }
+        }
+        result
+    }
+}
+
+fn main() {
+    let tree = Some(Rc::new(RefCell::new(TreeNode {
+        val: 1,
+        left: None,
+        right: Some(Rc::new(RefCell::new(TreeNode {
+            val: 2,
+            right: None,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 3,
+                left: None,
+                right: None,
+            }))),
+        }))),
+    })));
+    println!("{:?}", Solution::inorder_traversal(tree));
+}
